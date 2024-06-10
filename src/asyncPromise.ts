a1c0916112080513f8edfcdf4e3e374e8baf2de4
xdoer <gotoanything@foxmail.com>
2024-06-10 21:32:29 +0800

    feat: add asyncPromise

--- src/asyncPromise.ts
+++ src/asyncPromise.ts
@@ -14,3 +14,23 @@ export class AsyncPromise<T = any, N = any> {
     })
   }
 }
+
+// 缓存 promise 状态，pending 状态下，多次触发，只会执行一次
+export function asyncPromise<T>(cb: (...args) => Promise<T>) {
+  let asyncPromise = new AsyncPromise<T>();
+
+  return async function (...args) {
+    if (asyncPromise.loading) return asyncPromise.promise;
+    asyncPromise.loading = true;
+    try {
+      const res = await cb(...args);
+      asyncPromise.resolve(res);
+      return asyncPromise.promise;
+    } catch (e) {
+      asyncPromise.reject(e);
+      const old = asyncPromise;
+      asyncPromise = new AsyncPromise();
+      return old.promise;
+    }
+  };
+}
